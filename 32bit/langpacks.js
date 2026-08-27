16016fdc93bc8fd354126d0e1c49c0480c0fd8ae
Dimitris Tzemos <dijemos@gmail.com>
2026-08-27 12:46:35 +0300

    32bit script

---
+++ 32bit/langpacks.js
@@ -0,0 +1,5 @@
+// This forces Thunderbird language to switch to locale of machine
+// if available.
+pref("intl.locale.requested", "");
+pref("intl.multilingual.enabled",true);
+pref("extensions.langpacks.signatures.required",false);
